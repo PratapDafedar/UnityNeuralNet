d105917fbf86b12b9fc3c3169a89b51e4e2d428b
pratapdafedar <pratapdafedar@gmail.com>
2018-01-03 23:03:02 +0530

    Working some optimised core rendering funtion library.

---
+++ Assets/NeuralNetwork/GLRender/GLHelper.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace NeuralNetwork.GLRendering
+{
+	public static class GLHelper
+	{
+		public static void BeginRendering(Matrix4x4 matrix)
+		{
+			GL.PushMatrix(); 
+			GL.MultMatrix(matrix);
+		}
+
+		public static void EndRendering()
+		{
+			GL.PopMatrix();
+		}
+
+		public static void DrawLine(Vector3 pointA, Vector3 pointB, Color color)
+		{
+			GL.Color(color);
+			GL.Vertex(pointA); 
+			GL.Vertex(pointB);
+		}
+
+		public static void DrawCube(Vector3 point, float scale, Color color)
+		{
+			DrawCuboid (point, new Vector3 (scale, scale, scale), color);
+		}
+
+		public static void DrawCuboid(Vector3 p, Vector3 s, Color color)
+		{//purposefully reverse faced cuboid.
+			GL.Color(color);
+			GL.Vertex3(p.x - s.x, p.y + s.y, p.z - s.z);
+			GL.Vertex3(p.x + s.x, p.y + s.y, p.z - s.z);
+			GL.Vertex3(p.x + s.x, p.y - s.y, p.z - s.z);
+			GL.Vertex3(p.x - s.x, p.y - s.y, p.z - s.z);
+			GL.Vertex3(p.x + s.x, p.y + s.y, p.z - s.z);
+			GL.Vertex3(p.x + s.x, p.y + s.y, p.z + s.z);
+			GL.Vertex3(p.x + s.x, p.y - s.y, p.z + s.z);
+			GL.Vertex3(p.x + s.x, p.y - s.y, p.z - s.z);
+			GL.Vertex3(p.x + s.x, p.y + s.y, p.z + s.z);
+			GL.Vertex3(p.x - s.x, p.y + s.y, p.z + s.z);
+			GL.Vertex3(p.x - s.x, p.y - s.y, p.z + s.z);
+			GL.Vertex3(p.x + s.x, p.y - s.y, p.z + s.z);
+			GL.Vertex3(p.x - s.x, p.y + s.y, p.z + s.z);
+			GL.Vertex3(p.x - s.x, p.y + s.y, p.z - s.z);
+			GL.Vertex3(p.x - s.x, p.y - s.y, p.z - s.z);
+			GL.Vertex3(p.x - s.x, p.y - s.y, p.z + s.z);
+			GL.Vertex3(p.x - s.x, p.y - s.y, p.z - s.z);
+			GL.Vertex3(p.x + s.x, p.y - s.y, p.z - s.z);
+			GL.Vertex3(p.x + s.x, p.y - s.y, p.z + s.z);
+			GL.Vertex3(p.x - s.x, p.y - s.y, p.z + s.z);
+			GL.Vertex3(p.x - s.x, p.y + s.y, p.z + s.z);
+			GL.Vertex3(p.x + s.x, p.y + s.y, p.z + s.z);
+			GL.Vertex3(p.x + s.x, p.y + s.y, p.z - s.z);
+			GL.Vertex3(p.x - s.x, p.y + s.y, p.z - s.z);
+		}
+	}
+}
